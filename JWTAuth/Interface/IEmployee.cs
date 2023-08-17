9abb0b4ef2eabdaa86c70de0903f241a39178a0a
ramadhaniabidin12@gmail.com <ramadhaniabidin12@gmail.com>
2023-08-17 18:54:17 +0700

    add Interface for Employee and implemented it in Repository

---
+++ JWTAuth/Interface/IEmployee.cs
@@ -0,0 +1,14 @@
+﻿using JWTAuth.Models;
+
+namespace JWTAuth.Interface
+{
+    public interface IEmployee
+    {
+        public List<Employee> GetEmployeeDetails();
+        public Employee GetEmployeeDetails(int id);
+        public void AddEmployee(Employee employee);
+        public void UpdateEmployee(Employee employee);
+        public Employee DeleteEmployee(int id);
+        public bool CheckEmployee(int id);
+    }
+}
---
+++ JWTAuth/Repository/EmployeeRepository.cs
@@ -0,0 +1,102 @@
+﻿using JWTAuth.Interface;
+using JWTAuth.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTAuth.Repository
+{
+    public class EmployeeRepository : IEmployee
+    {
+        readonly DatabaseContext _dbContext = new();
+        public EmployeeRepository(DatabaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            try
+            {
+                _dbContext.Employees.Add(employee);
+                _dbContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public bool CheckEmployee(int id)
+        {
+            return _dbContext.Employees.Any(e => e.EmployeeID == id);
+        }
+
+        public Employee DeleteEmployee(int id)
+        {
+            try
+            {
+                Employee? employee = _dbContext.Employees.Find(id);
+
+                if (employee != null)
+                {
+                    _dbContext.Employees.Remove(employee);
+                    _dbContext.SaveChanges();
+                    return employee;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<Employee> GetEmployeeDetails()
+        {
+            try
+            {
+                return _dbContext.Employees.ToList();
+            }
+
+            catch
+            {
+                throw;
+            }
+        }
+
+        public Employee GetEmployeeDetails(int id)
+        {
+            try
+            {
+                Employee? employee = _dbContext.Employees.Find(id);
+                if (employee != null)
+                {
+                    return employee;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            try
+            {
+                _dbContext.Entry(employee).State = EntityState.Modified;
+                _dbContext.SaveChanges();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
